5efa8a1960b48e4a66e49ef82b49d201ee640d13
Michael Welford <michael.welford@adelaide.edu.au>
2018-04-01 08:45:24 +0930

    All working. Add in README.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# autoload-drupal
-Composer plugin to add Drupal autoloading to the composer autoloader
+# Drupal 8 Composer Autoloader
+
+A Composer plugin to add Drupal 8 autoloading of modules to the composer autoloader.
+
+## Why?
+
+Why would you want this? Doesn't Drupal 8 have it's own autoloading mechanism?
+
+There are quite a few handy types of tools (e.g. static analysis, intellisense) that rely on being able to load all classes via the composer autoloader. Unfortunately, as Drupal 8 does it's own autoloading at boot time, using these tools at best becomes slow (if you try to boot Drupal on the fly) or at worst becomes impossible (because you are running Drupal inside a VM). Some IDEs (e.g. PHPStorm) get around this by implementing their own discovery mechanism but if you are using an IDE or editor that doesn't do this you are out of luck.
+
+This plugin plugs this gap.
+
+## How
+
+This plugin is heavily based off of the (Composer Merge Plugin)[https://github.com/wikimedia/composer-merge-plugin]. Essentially this plugin creates a `composer.json` file in memory including in all the specified modules to an `autoload` section and merges it into the root `composer.json` file.
+
+## Installation
+
+### Require the plugin in your `composer.json`
+
+Standard stuff:
+
+```json
+{
+    "require": {
+        "fenetikm/autoload-drupal": "dev-master"
+    }
+}
+```
+
+### Configure modules to autoload
+
+This plugin is configured via the `extra` section in your `composer.json`. Usually you would want the `app/modules/contrib/`, `app/core/modules/` and the `app/modules/custom/` directories included. As Drupal can be configured in many ways, none of this is assumed and so all must be added in.
+
+You can also constrain which modules are added in from a directory by specifying an array of the pattern `[ "directory_to_include", [ "module1", "module2" ] ]`.
+
+For example:
+
+```json
+    "extra": {
+        "autoload-drupal": {
+            "modules": [
+              "app/modules/contrib/",
+              "app/core/modules/",
+              [
+                  "app/modules/custom/", [ "my_module" ]
+              ]
+            ]
+        }
+    }
+
+```
+
+Here, all modules in `app/modules/contrib/` are added in, all modules in `app/core/modules/` are added in and only the `my_module` module is added in from the `app/modules/custom/` directory.
---
+++ src/DrupalAutoloadPlugin.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace fenetikm;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Package\Loader\ArrayLoader;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event as ScriptEvent;
+use Composer\Script\ScriptEvents;
+
+/**
+ * Composer plugin that pulls in Drupal modules to the autoloader.
+ */
+class DrupalAutoloadPlugin implements PluginInterface, EventSubscriberInterface {
+
+  /**
+   * Priority that plugin uses to register callbacks.
+   *
+   * @var int
+   */
+  const CALLBACK_PRIORITY = 50000;
+
+  /**
+   * Composer object.
+   *
+   * @var \Composer\Composer
+   */
+  protected $composer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    $this->composer = $composer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return array(
+      ScriptEvents::PRE_AUTOLOAD_DUMP =>
+      array('onInstallUpdateOrDump', self::CALLBACK_PRIORITY),
+      ScriptEvents::PRE_INSTALL_CMD =>
+      array('onInstallUpdateOrDump', self::CALLBACK_PRIORITY),
+      ScriptEvents::PRE_UPDATE_CMD =>
+      array('onInstallUpdateOrDump', self::CALLBACK_PRIORITY),
+    );
+  }
+
+  /**
+   * Install / update or dump event handler.
+   *
+   * @param \Composer\Script\Event $event
+   *   The event.
+   */
+  public function onInstallUpdateOrDump(ScriptEvent $event) {
+    $this->mergeAutoload();
+  }
+
+  /**
+   * Build the namespace array for a modules entry.
+   *
+   * @param array $namespaces
+   *   The current namespaces array to add into.
+   * @param string $base_dir
+   *   The base directory to build from.
+   * @param array $constrain
+   *   Modules to constrain to.
+   */
+  public function buildNamespace(array &$namespaces, $base_dir, array $constrain = []) {
+    // Add in trailing slash if missing.
+    if ($base_dir[strlen($base_dir) - 1] != '/') {
+      $base_dir .= '/';
+    }
+
+    $filter_out = ['.', '..'];
+    $dir_exists = file_exists($base_dir);
+    if (!$dir_exists) {
+      return;
+    }
+
+    $files = scandir($base_dir);
+    if ($files === FALSE) {
+      return;
+    }
+
+    $module_directories = array_filter($files, function ($item) use ($filter_out, $base_dir, $constrain) {
+      // Only insert modules with src directories.
+      $has_src = file_exists($base_dir . $item . '/src');
+
+      // Check against specific modules if applicable.
+      $is_module = TRUE;
+      if (!empty($constrain)) {
+        $is_module = in_array($item, $constrain);
+      }
+
+      return $has_src &&
+        $is_module &&
+        !in_array($item, $filter_out) &&
+        is_dir($base_dir . $item);
+    });
+
+    array_map(function ($directory) use ($base_dir, &$namespaces) {
+      $this->addModuleToNamespaces($namespaces, $directory, $base_dir);
+    }, $module_directories);
+  }
+
+  /**
+   * Add module to namespaces.
+   *
+   * @param array $namespaces
+   *   The current namespaces array to add into.
+   * @param string $directory
+   *   The directory of the module.
+   * @param string $base_dir
+   *   The base directory to build from.
+   */
+  public function addModuleToNamespaces(array &$namespaces, $directory, $base_dir) {
+    $namespace = 'Drupal\\' . $directory . '\\';
+    $namespaces[$namespace] = $base_dir . $directory . '/src';
+  }
+
+  /**
+   * Build the namespaces array.
+   */
+  public function buildNamespaces() {
+    $extra = $this->composer->getPackage()->getExtra();
+    $modules = isset($extra['autoload-drupal']['modules']) ? $extra['autoload-drupal']['modules'] : [];
+    $namespaces = [];
+    array_map(function ($module) use (&$namespaces) {
+      $constrain = [];
+      $base_dir = $module;
+      if (is_array($module)) {
+        $constrain = isset($base_dir[1]) ? $base_dir[1] : [];
+        $base_dir = $base_dir[0];
+      }
+      return $this->buildNamespace($namespaces, $base_dir, $constrain);
+    }, $modules);
+
+    return $namespaces;
+  }
+
+  /**
+   * Build the autoload json to merge in.
+   */
+  public function buildAutoloadJson() {
+    $composer_output = [
+      'name' => 'fenetikm/autoload-drupal',
+      'version' => '1.0',
+      'autoload' => [
+        'psr-4' => $this->buildNamespaces(),
+      ],
+    ];
+
+    $composer_json = json_encode($composer_output, JSON_PRETTY_PRINT);
+    return $composer_json;
+  }
+
+  /**
+   * Merge in the built autoload json.
+   */
+  public function mergeAutoload() {
+    $root = $this->composer->getPackage();
+    $new_autoload_json = $this->buildAutoloadJson();
+    $new_autoload = json_decode($new_autoload_json, TRUE);
+    $loader = new ArrayLoader();
+    $package = $loader->load($new_autoload);
+
+    $autoload = $package->getAutoload();
+    $root->setAutoload(array_merge_recursive(
+      $root->getAutoload(),
+      $autoload
+    ));
+  }
+
+}
